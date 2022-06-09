ca185b4c4b280c1048b9733ae33d6cf606800fd4
stonerchef <95292678+stonerchef@users.noreply.github.com>
2022-06-09 13:57:22 +0200

    Add files via upload

---
+++ Barefoot-Killer/desktop/src/com/stonerchef/barefoot/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.stonerchef.barefoot;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.stonerchef.barefoot.main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Barefoot-Killer");
+		new Lwjgl3Application(new main(), config);
+	}
+}
