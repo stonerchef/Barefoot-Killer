225eaf30d9f3f8436d98cd8ebed14ab62ad45442
S4bril <95976169+S4bril@users.noreply.github.com>
2022-06-13 20:31:52 +0200

    Add files via upload

---
+++ Barefoot-Killer/core/src/com/bfkiller/game/GameMap.java
@@ -0,0 +1,18 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import java.util.ArrayList;
+public class GameMap {
+    ArrayList<Enemies> monsters;
+    public GameMap() {
+        monsters = new ArrayList<>();
+        for(int i = 0; i < 20; i++){
+            monsters.add(new Enemies());
+        }
+    }
+    public void draw(ShapeRenderer shape){
+        for (Enemies monster : monsters){
+            monster.draw(shape);
+        }
+    }
+}
