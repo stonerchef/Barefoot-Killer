79eb39f643babed519297e471b8f137a5efe7623
S4bril <95976169+S4bril@users.noreply.github.com>
2022-06-13 16:20:22 +0200

    Update Enemies.java

--- Barefoot-Killer/core/src/com/bfkiller/game/Enemies.java
+++ Barefoot-Killer/core/src/com/bfkiller/game/Enemies.java
@@ -1,4 +1,9 @@
-package com.mygdx.game;
+package com.bfkiller.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import java.util.Random;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -8,38 +13,17 @@ import java.util.Random;
 public class Enemies {
     int posX;
     int posY;
-    int speedX;
-    int speedY;
+    int movementSpeedPreSec;
     int size;
     public Enemies() {
         Random randInt = new Random();
-        speedX = 5;
-        speedY = 5;
+        movementSpeedPreSec = 240;
         size = 10;
         posX = randInt.nextInt(Gdx.graphics.getWidth() * 2 ) - Gdx.graphics.getWidth() / 2;
         posY = randInt.nextInt(Gdx.graphics.getHeight() * 2 ) - Gdx.graphics.getHeight() / 2;
     }
-
-    public void update() {
-        if(posX <= size || posX >= Gdx.graphics.getWidth() - size) speedX *= -1;
-        if(posY <= size || posY >= Gdx.graphics.getHeight() - size) speedY *= -1;
-        posX += speedX;
-        posY += speedY;
-    }
-    public void moveLeft() {
-        posX -= 5;
-    }
-    public void moveRight(){
-        posX += 5;
-    }
-    public void moveTop() {
-        posY += 5;
-    }
-    public void moveBot(){
-        posY -= 5;
-    }
-
     public void draw(ShapeRenderer shape) {
         shape.circle(posX, posY, size);
     }
+}
 }
